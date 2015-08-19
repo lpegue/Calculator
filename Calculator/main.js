ecd5eba22f7beb0cc68436e22d0d5a4378b1c8d0
Leslie Pegue <lpegue4@gmail.com>
2015-08-18 20:15:59 -0700

    commit to add for loop

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,7 +1,9 @@
-var array
+var leslie = document.querySelectorAll(".buttons"); // go select all items in the HTML with class "buttons and shove them in the array "leslie"
 
 
-
-
-document.getElementById"buttons".setEventHandler("click" function)
-	
+for (var i =0 ; i < leslie.length; i++ ) { //cycle through everything in the leslie array
+	leslie[i].addEventListener("click", //attach an event listener to each item
+		function (){
+			console.log(this.innerHTML); // console.log that item's inner HTML when clcked
+		})
+}
